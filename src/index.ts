6b3c2c5be35f80a8092b1f0c69892d3e48e768c7
Pedro Valentim <pedro.santos@olist.com>
2026-04-09 13:02:07 -0300

    criacao dos arquivos para rodar em nextJS

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { MenuErp } from './components/MenuErp';
+export { Button } from './components/Button';
+export { Checkbox } from './components/Checkbox';
+export { Logo } from './components/Logo';
+export { MenuSidebar } from './components/MenuSidebar';
+export { SegmentedButtons } from './components/SegmentedButtons';
